083df933ee45b2093cd28156dcbb5ccff86ae041
Eduardo <eduardotakeuchi@outlook.com>
2024-05-04 00:17:47 -0300

    comments

--- explication.py
+++ explication.py
@@ -1,44 +1,68 @@
+# Importing the tkinter module and aliasing it as tk
 import tkinter as tk
 
+# Creating the main application window
 root = tk.Tk()
-root.resizable(False, False) 
-root.title("Tic Tac Toe")
 
+# Making the window not resizable
+root.resizable(True, False) # Change the first parameter to True to make the window resizable in the x-direction
+
+# Setting the title of the window
+root.title("Tic Tac Toe for English")
+
+# Creating a label widget with the title "Tic Tac Toe" and packing it into the root window
 tk.Label(root, text="Tic Tac Toe", font=('Ariel', 25)).pack()
+
+# Creating a label widget to display game status and packing it into the root window
 status_label = tk.Label(root, text="X's turn", font=('Ariel', 15), bg='green', fg='snow')
 status_label.pack(fill=tk.X)
+
+# Function to reset the game
 def play_again():
     global current_chr
-    current_chr = 'X'
-    for point in XO_points:
-        point.button.configure(state=tk.NORMAL)
-        point.reset()
-    status_label.configure(text="X's turn")
-    play_again_button.pack_forget()
-play_again_button = tk.Button(root, text='Play again', font=('Ariel', 15), command=play_again)
+    current_chr = 'X' # First player is always X
+    # Resetting all points
+    for point in XO_points: # XO_points is a list of all points in the game
+        point.button.configure(state=tk.NORMAL) # Making the point clickable
+        point.reset() 
+    status_label.configure(text="X's turn") # Setting the status label to show that it's X's turn
+    play_again_button.pack_forget() # Hiding the play again button
 
+# Creating a button widget to play again
+play_again_button = tk.Button(root, text='Play again', font=('Ariel', 15), command=play_again)
 play_with = "Computer"
+# Function to set the game mode to play with a human
 def play_with_human():
-    global play_with
-    play_with = "Human"
+    global play_with 
+    play_with = "Human" 
     play_with_button['text'] = "Play with computer"
     play_with_button['command'] = play_with_computer
     play_again()
+
+# Function to set the game mode to play with a computer
 def play_with_computer():
     global play_with
     play_with = "Computer"
     play_with_button['text'] = "Play with human"
     play_with_button['command'] = play_with_human
     play_again()
+
+# Creating a button widget to choose to play with a human initially
 play_with_button = tk.Button(root, text='Play with human', font=('Ariel', 15), command=play_with_human)
 play_with_button.pack()
 
+# Variable to keep track of the current player
 current_chr = "X"
 
+# Creating the play area frame
 play_area = tk.Frame(root, width=300, height=300, bg='white')
+
+# Lists to keep track of X and O points
 XO_points = []
 X_points = []
 O_points = []
+
+# Class to represent each point on the game grid
 class XOPoint:
     def __init__(self, x, y):
         self.x = x
@@ -47,11 +71,13 @@ class XOPoint:
         self.button = tk.Button(play_area, text="", width=10, height=5, command=self.set)
         self.button.grid(row=x, column=y)
 
+    # Function to set the value of the point
     def set(self):
         global current_chr
         if not self.value:
             self.button.configure(text=current_chr, bg='snow', fg='black')
             self.value = current_chr
+            # Updating the player's turn
             if current_chr == "X":
                 X_points.append(self)
                 current_chr = "O"
@@ -60,10 +86,12 @@ class XOPoint:
                 O_points.append(self)
                 current_chr = "X"
                 status_label.configure(text="X's turn")
+        # Checking for a win or draw
         check_win()
         if play_with == "Computer" and status_label['text'] == "O's turn":
             auto_play()
 
+    # Function to reset the point
     def reset(self):
         self.button.configure(text="", bg='lightgray')
         if self.value == "X":
@@ -71,17 +99,23 @@ class XOPoint:
         elif self.value == "O":
             O_points.remove(self)
         self.value = None
+
+# Creating XOPoint objects for each cell in the grid
 for x in range(1, 4):
     for y in range(1, 4):
         XO_points.append(XOPoint(x, y))
+
+# Class to represent winning possibilities
 class WinningPossibility:
     def __init__(self, x1, y1, x2, y2, x3, y3):
-        self.x1 = x1
-        self.y1 = y1
-        self.x2 = x2
-        self.y2 = y2
-        self.x3 = x3
-        self.y3 = y3
+        self.x1 = x1 # x-coordinate of the first point
+        self.y1 = y1 # y-coordinate of the first point
+        self.x2 = x2 # x-coordinate of the second point
+        self.y2 = y2 # y-coordinate of the second point
+        self.x3 = x3 # x-coordinate of the third point
+        self.y3 = y3 # y-coordinate of the third point
+
+    # Function to check if a winning possibility is satisfied
     def check(self, for_chr):
         self.p1_satisfied = False
         self.p2_satisfied = False
@@ -103,6 +137,8 @@ class WinningPossibility:
                 elif point.x == self.x3 and point.y == self.y3:
                     self.p3_satisfied = True
         return all([self.p1_satisfied, self.p2_satisfied, self.p3_satisfied])
+
+# List of winning possibilities
 winning_possibilities = [
     WinningPossibility(1, 1, 1, 2, 1, 3),
     WinningPossibility(2, 1, 2, 2, 2, 3),
@@ -113,10 +149,14 @@ winning_possibilities = [
     WinningPossibility(1, 1, 2, 2, 3, 3),
     WinningPossibility(3, 1, 2, 2, 1, 3)
 ]
+
+# Function to disable the game after it's finished
 def disable_game():
     for point in XO_points:
         point.button.configure(state=tk.DISABLED)
     play_again_button.pack()
+
+# Function to check if the game has been won or drawn
 def check_win():
     for possibility in winning_possibilities:
         if possibility.check('X'):
@@ -130,10 +170,13 @@ def check_win():
     if len(X_points) + len(O_points) == 9:
         status_label.configure(text="Draw!")
         disable_game()
+
+# Packing the play area into the root window
 play_area.pack(pady=10, padx=10)
 
 
 def auto_play():
+
 
     # If winning is possible in the next move
     for winning_possibility in winning_possibilities:
